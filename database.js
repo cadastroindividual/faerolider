560504bfe6df0fd2fc2b37d36f9fb362aa97dd01
cadastroindividual <cadastroindividual@lider-saude.com>
2026-02-17 22:55:04 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDVAf8N6g6zD8VR_TPzQKMsA-eIGQuT0AQ",
-  authDomain: "frutiger-aero-lider.firebaseapp.com",
-  projectId: "frutiger-aero-lider",
-  storageBucket: "frutiger-aero-lider.appspot.com"
+const firebaseConfig={
+apiKey:"AIzaSyDVAf8N6g6zD8VR_TPzQKMsA-eIGQuT0AQ",
+authDomain:"frutiger-aero-lider.firebaseapp.com",
+projectId:"frutiger-aero-lider",
+storageBucket:"frutiger-aero-lider.appspot.com"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+const db=firebase.firestore();
