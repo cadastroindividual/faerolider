59796fb46d5475fe64e726952b1a21eacfb3664f
cadastroindividual <cadastroindividual@lider-saude.com>
2026-02-17 23:10:33 -0400

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig={
-apiKey:"AIzaSyDVAf8N6g6zD8VR_TPzQKMsA-eIGQuT0AQ",
-authDomain:"frutiger-aero-lider.firebaseapp.com",
-projectId:"frutiger-aero-lider",
-storageBucket:"frutiger-aero-lider.appspot.com"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db=firebase.firestore();
