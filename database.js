4380e4b034d3f781d13de7d208aef56ec373c0a5
cadastroindividual <cadastroindividual@lider-saude.com>
2026-02-18 00:27:47 -0400

    Update database.js

--- database.js
+++ database.js
@@ -2,6 +2,14 @@
 // FRUTIGER AERO LÍDER — Firebase Config
 // ============================================
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDJT35OkIJE7nvdndaBcUzuPYIf3S0SbNo",
   authDomain: "frutigeraero-lider.firebaseapp.com",
@@ -15,6 +23,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+
+
 // ── Presença em tempo real ──────────────────
 auth.onAuthStateChanged(user => {
   if (!user) return;
